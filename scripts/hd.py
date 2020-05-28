f45c3f8d6c1f1922e641592f0bae32c02dbe223f
nolimitcarter <carterthayer@protonmail.com>
2020-05-27 21:39:35 -0400

    alacritty

---
+++ scripts/hd.py
@@ -0,0 +1,49 @@
+# Hexdump script
+# python3 hd.py myfile.txt
+
+import sys
+
+dorscheme gruvboief prinne(asc)tLount, last=True)
+
+
+if __name__ == "__main__":
+        main()
+                        for char in line:
+                        ascune in fileN:
+                                        for char in line:
+                                                            ascppend(char)
+
+                                                                                if len(asc)nt, last=False):
+        print("{0:0>8}".format(format(count, "X")) + " ", end="")
+
+            byteString = ""
+
+                for byte in bytes:
+                            byteString += ("{0:0>2}".format(format(ord(byte), "X")) + " ")
+
+                                print("{:<48}".format(byteString), end="")
+
+                                    out = "|" + "".join(bytes) + "|"
+
+                                        whiteSpace = ["\n", "\t"]
+
+                                            for char in out:
+                                                        if char in whiteSpace:
+                                                                        out = out.replace(char, ".")
+
+                                                                            if last == True:
+ne(asc)iolount)
+                        totalCount += 1
+                                                asc
+                                                                                        print("{:<13}".format(out))
+                                                                                            else:
+                                                                                                        print(out)
+
+                                                                                                        def main():
+
+                                                                                                                for fileName in sys.argv[1:]:
+                                                                                                                            print("\n{} hexdump".format(fileName))
+                                                                                                                                    totalCount = 0
+
+                                                                                                                                            with open(fileName) as fileN:
+                                                                                                                                                asc
