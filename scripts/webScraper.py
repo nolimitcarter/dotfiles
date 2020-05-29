3d2a2e2c17c80cf47515a16015f7764fb885c020
nolimitcarter <carterthayer@protonmail.com>
2020-05-29 01:25:25 -0400

    added some comments and explanations

--- scripts/webScraper.py
+++ scripts/webScraper.py
@@ -7,7 +7,11 @@ page = requests.get(URL)
 
 soup = BeautifulSoup(page.content, 'html.parser')
     
+# enter specific html element by id: 
 results = soup.find(id='')
 
+# print results
 print(results.prettify())
 
+# you are also able to fine elements by class name: 
+# ex: job_elems = results.find.all('section', class_='card-content')
