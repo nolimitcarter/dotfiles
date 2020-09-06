ee1e698883060a5d21cce74198c127567b80e05a
carterthayer@protonmail.com <carterthayer@protonmail.com>
2020-09-05 20:39:29 -0400

    added

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,24 +1,13 @@
 #!/usr/bin/env bash
 
 # Terminate already running bar instances
-#killall -q polybar
-
-# Wait until the processes have been shut down
-#while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
-
-#for m in $(polybar --list-monitors | cut -d":" -f1); do
-#	WIRELESS=$(ls /sys/class/net/ | grep ^wl | awk 'NR==1{print $1}') MONITOR=$m polybar --reload mainbar-i3 &
-#done
-
-#echo "Bars launched..."
-
-# Terminate already running bar instances
 killall -q polybar
+# If all your bars have ipc enabled, you can also use 
+# polybar-msg cmd quit
 
-# Wait until the processes have been shut down
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+# Launch bar1 and bar2
+echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
+polybar bar1 >>/tmp/polybar1.log 2>&1 & disown
+polybar bar2 >>/tmp/polybar2.log 2>&1 & disown
 
-# Launch Polybar, using default config location ~/.config/polybar/config
-polybar --reload example &
-
-echo "Polybar launched..."
+echo "Bars launched..."
