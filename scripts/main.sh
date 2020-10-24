47d5fd6bc653e2b0049e8ff227eeddd03d0a0226
carterthayer@protonmail.com <carterthayer@protonmail.com>
2020-10-23 23:43:00 -0400

    added start

--- scripts/main.sh
+++ scripts/main.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DisplayPort-0 --off --output DisplayPort-1 --mode 1920x1080 --pos 0x89 --rotate right --output DisplayPort-2 --mode 1920x1080 --pos 1080x0 --rotate normal --output HDMI-A-0 --mode 1920x1080 --pos 1080x1080 --rotate normal
+xrandr --output DisplayPort-0 --off --output DisplayPort-1 --mode 1920x1080 --pos 0x89 --rotate right --output DisplayPort-2 --mode 1920x1080 --pos 1080x0 --rotate normal 
