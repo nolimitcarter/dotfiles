548595948f7f52adf8d16fb713e1f7352412b6f0
nolimitcarter <carterthayer@protonmail.com>
2021-01-06 03:09:22 -0500

    added mac font change

---
+++ setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+ln -sf ~/repos/github.com/nolimitcarter/dotfiles/vim/vimrc ~/.vimrc && ln -sf ~/repos/github.com/nolimitcarter/dotfiles/bash/bashrc ~/.bashrc && ~/repos/github.com/nolimitcarter/dotfiles/bash/bash_aliases ~/.bash_aliases && ~/repos/github.com/nolimitcarter/dotfiles/alacritty/alacritty.yml  ~/.config/alacritty.yml && ln -sf ~/repos/github.com/nolimitcarter/dotfiles/tmux/tmux.conf ~/.tmux.conf 
+
+## should i put i3 setup in here?
