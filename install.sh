a7b58e2b79966df22477ffc5c2fb69b2e4e64db9
carterthayer@protonmail.com <carterthayer@protonmail.com>
2021-03-09 20:16:31 -0500

    live testing...

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# !! Does not include git cloning the repository.
+# Make sure you run this script from the directory containting it.
 
 ln -sf ~/repos/github.com/nolimitcarter/dotfiles/vim/vimrc ~/.vimrc && ln -sf ~/repos/github.com/nolimitcarter/dotfiles/bash/bashrc ~/.bashrc && ~/repos/github.com/nolimitcarter/dotfiles/bash/bash_aliases ~/.bash_aliases && ~/repos/github.com/nolimitcarter/dotfiles/alacritty/alacritty.yml  ~/.config/alacritty.yml && ln -sf ~/repos/github.com/nolimitcarter/dotfiles/tmux/tmux.conf ~/.tmux.conf 
 
